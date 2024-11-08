97f2a5097587c9eb76fbb81745608f103193bc89
zchelalo <eduardosaavedra687@gmail.com>
2024-11-07 21:47:04 -0700

    add: router and repository of class

--- internal/modules/class/infrastructure/adapters/grpc/grpc.go
+++ internal/modules/class/infrastructure/adapters/grpc/grpc.go
@@ -1,1 +1,21 @@
 package classGRPC
+
+// import (
+// 	classPostgres "github.com/zchelalo/sa_class_management/internal/modules/class/infrastructure/repositories/postgres"
+// 	classProto "github.com/zchelalo/sa_class_management/pkg/proto/class"
+// 	"github.com/zchelalo/sa_class_management/pkg/sqlc/db"
+// )
+
+// type ClassRouter struct {
+// 	useCase *classApplication.ClassUseCases
+// 	classProto.UnimplementedClassServiceServer
+// }
+
+// func New(store *db.SQLStore) *ClassRouter {
+// 	classRepository := classPostgres.New(store)
+// 	classUseCases := classApplication.New(classRepository)
+
+// 	return &ClassRouter{
+// 		useCase: classUseCases,
+// 	}
+// }
---
+++ internal/modules/class/infrastructure/repositories/postgres/postgres.go
@@ -0,0 +1,13 @@
+package classPostgres
+
+// import "github.com/zchelalo/sa_class_management/pkg/sqlc/db"
+
+// type PostgresRepository struct {
+// 	store *db.SQLStore
+// }
+
+// func New(store *db.SQLStore) classDomain.ClassRepository {
+// 	return &PostgresRepository{
+// 		store: store,
+// 	}
+// }
